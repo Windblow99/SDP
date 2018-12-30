b7a82e524050fc05a3cab89ec0651def43fb14fa
jasonnjs <45647571+jasonnjs@users.noreply.github.com>
2018-12-30 19:01:06 +0800

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="icon" type="image/ico" href="/pictures/logo.jpg" />
-<title>educo</title>
+<title>Log In</title>
 <script>
 	function viewpassword()
 	{
