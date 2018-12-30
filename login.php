62151e7eb06ed376ccfcd7a176383f6be997dcd7
jasonnjs <45647571+jasonnjs@users.noreply.github.com>
2018-12-30 10:15:09 +0800

    Update login.php

--- login.php
+++ login.php
@@ -58,7 +58,7 @@
 				<h1 style="color:#990000">Log In</h1><br/><br/>
 				<h4>Hello there! We are excited to see you again!</h4>
 				<br/><br/><br/>
-					<form method="post" action="loginfunction.php">
+					<form method="post" action="#">
 						<table>
 							<tr><th><br/>E-mail: </th><td><br/><input id="id" type="text" name="userid" required="required" style="margin-left: 20px; width: 225px; height: 25px;"/></td></tr>
 							<tr><th><br/><br/><br/>Password: </th><td><br/><br/><br/><input id="password" type="password" name="password" required="required" style="margin-left: 20px; width: 225px; height: 25px;"/> &nbsp; &nbsp; <input type="checkbox" onclick="viewpassword()"/> View Password</td></tr>
