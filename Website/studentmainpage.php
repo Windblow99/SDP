b3706e18637d2d657106b281c9dac50556f10976
jasonnjs <45647571+jasonnjs@users.noreply.github.com>
2019-01-07 22:14:18 +0800

    1

---
+++ Website/studentmainpage.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<?php include($_SERVER["DOCUMENT_ROOT"].'/template/header.php'); ?>
+<title>educo</title>
+</head>
+
+<body>
+
+		<?php include($_SERVER["DOCUMENT_ROOT"].'/template/navbar.php'); ?>
+		<!--<div id="header">
+				<a href="#" class="logo">educo</a>
+				<a href="#" class="nav">Home</a>
+				<a href="#" class="nav">About</a>
+				<a href="#" class="nav">Exams</a>
+				<a href="#" class="nav">Lab Session</a>
+				<a href="#" class="active">Certificates</a>
+				<label>Welcome back,<a href="#" class="account">Account</a></label>
+
+		</div>-->
+
+		<div id="page-content">
+		<center>
+	
+		<h2>Testing</h2>
+
+
+	
+		</center>
+		</div>
+		
+		<!--<div id="footer">
+			<a href="#">Feedback</a> | Copyright 2018 educo Malaysia
+		</div>-->
+
+
+</body>
+
+</html>
---
+++ template/navbartesting.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<!--<?php include "header.php"; ?>-->
+<?php include($_SERVER["DOCUMENT_ROOT"].'/template/header.php'); ?>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<title>educo</title>
+<link rel="stylesheet" type="text/css" href="CSS/Main CSS.css">
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-lg sticky-top" style="background-color: #e3f2fd;">
+  <a class="navbar-brand" href="#">Educo</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Student</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Exams</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Lab Sessions</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Certificates</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Class Details</a>
+      </li>
+    </ul>
+
+    <ul class="navbar-nav">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Welcome Back, Eren.
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Account Settings</a>
+          <a class="dropdown-item" href="#">Account Details</a>
+          <a class="dropdown-item" href="#">Log Out</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+    <div id="page-content">
+    <center>
+  
+    <h2>Testing</h2>
+
+
+  
+    </center>
+    </div>
+    
+    <!--<div id="footer">
+      <a href="#">Feedback</a> | Copyright 2018 educo Malaysia
+    </div>-->
+
+
+</body>
+
+</html>
