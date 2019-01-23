885404f0ea1e16ac37eb931056c364f415ff31ba
jasonnjs <45647571+jasonnjs@users.noreply.github.com>
2019-01-24 04:45:55 +0800

    Update studentclassdetails.php

--- website/student/studentclassdetails.php
+++ website/student/studentclassdetails.php
@@ -5,7 +5,7 @@
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <?php include($_SERVER["DOCUMENT_ROOT"].'/template/header.php'); ?>
-<title>educo</title>
+<title>Class Details</title>
   <style>
   	body{
   		background-image: url("/Pictures/11.png");
@@ -22,72 +22,82 @@
 
 		<div id="page-content">
 			<div class="container">
-		<center>
+		
 	
-		<h1>CLASS DETAILS</h1> CLASS 1
+				<div class="row">
+
+				<div class="col-sm" style="font-size: 36px; color: #953031; font-weight: bold;"><br/>
+				CLASS DETAILS
+				</div>
+
+				<div class="col-sm" style="color: #ff1a8c; font-size: 18px;"><br/><br/>
+				<br/>CLASS 1
+				</div>
+				
+		</div>
 
 		<hr>
 
 			<div class="row">
 
 				<div class="col-sm">
-				<h5>Teacher in-charge:</h5>
+				<h5 style=" box-sizing: border-box; background-color: #ff1a8c; color: white; width: 300px; padding: 10px"><center>Teacher in-charge:</center></h5>
 				</div>
 
-				<div class="col-sm">
-				Eren Ong
+				<div class="col-sm"><br/>
+				<h5 style="font-weight: bold;">Eren Ong<h5>
 				</div>
 				
 			</div>		
 
 		<hr>
 
-		<h5>Current in-class students</h5>
+		<h5 style=" box-sizing: border-box; background-color: #ff1a8c; color: white; width: 300px; padding: 10px"><center>Current in-class students</center></h5><br/>
 
 			<div class="row">
 
-				<div class="col-sm">
+				<div class="col-sm" style="font-weight: bold;">
 				Student 1
 				</div>
 
-				<div class="col-sm">
+				<div class="col-sm" style="font-weight: bold;">
 				Student 2
 				</div>
 
-				<div class="col-sm">
+				<div class="col-sm" style="font-weight: bold;">
 				Student 3
 				</div>
 
-				<div class="col-sm">
+				<div class="col-sm" style="font-weight: bold;">
 				Student 4
 				</div>
 
-				<div class="col-sm">
+				<div class="col-sm" style="font-weight: bold;">
 				Student 5
 				</div>
 
-			</div>
+			</div><br/>
 
 			<div class="row">
 
-				<div class="col-sm">
-				Student 1
+				<div class="col-sm" style="font-weight: bold;">
+				Student 6
 				</div>
 
-				<div class="col-sm">
-				Student 2
+				<div class="col-sm" style="font-weight: bold;">
+				Student 7
 				</div>
 
-				<div class="col-sm">
-				Student 3
+				<div class="col-sm" style="font-weight: bold;">
+				Student 8
 				</div>
 
-				<div class="col-sm">
-				Student 4
+				<div class="col-sm" style="font-weight: bold;">
+				Student 9
 				</div>
 
-				<div class="col-sm">
-				Student 5
+				<div class="col-sm" style="font-weight: bold;">
+				Student 10
 				</div>
 
 			</div>
@@ -95,15 +105,40 @@
 
 		<hr>
 
-		<h5>Top Scores</h5>
-		This scoreboard only shows the Top 3 scorers.<br/><br/>
+		<div class="row">
+			<div class="col-sm"><br/>
+				<h5 style="box-sizing: border-box; background-color: #ff1a8c; color: white; width: 300px; padding: 10px"><center>Top Scores</center></h5>
+			</div>
 
-		1. Student 1<br/>
-		2. Student 2 <br/>
-		3. Student 3
+			<div class="col-sm"><br/>
+					Overall / Chapter:&nbsp;
+				<select>
+					<option value="chapter1">Chapter 1</option>
+ 					<option value="chapter2">Chapter 2</option>
+				</select>
+			</div>
+		</div>
+		<br/>
+
+		<label style="margin-left: 50px;">This scoreboard only shows the Top 3 scorers.</label><br/><br/>
+
+		
+
+		<div class="row">
+			<div class="col-sm-3">
+			<label style="font-weight: bold; color: #ffcc00; font-size: 30px;">1.</label>
+			<div>
+
+			<div class="col-sm-3" style="font-size: 30px;">
+			Student 1
+			</div>
+		</div>
+			2. Student 2 <br/>
+			3. Student 3
+		</div>
 
 	
-		</center>
+		
 			</div>
 		</div>
 		
